23b80cbe8e1b1a0bd1e06a6a041089aa4868408a
Sofia-Ortega <sofia.j.ortega@tamu.edu>
2020-09-19 16:23:11 -0500

    spaces bw chords now, fixed bug so actually outputs

--- chords.js
+++ chords.js
@@ -3,15 +3,16 @@
 const majorChords = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 
 
+
 let chordCombo = "";
 let randIndex;
 
 
 for(let i = 0; i < majorChords.length; i++) {
     randIndex = Math.floor(Math.random()*7);
-    chordCombo = chordCombo + majorChords[randIndex];
+    chordCombo = chordCombo + majorChords[randIndex] + " ";
 }
 
-document.getElementById("words").innerHTML = chordCombo;
+document.getElementById("chords").innerHTML = chordCombo;
 
 
