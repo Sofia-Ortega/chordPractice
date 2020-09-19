476a6538254224a385811d715fcc5a93d3419053
Sofia-Ortega <sofia.j.ortega@tamu.edu>
2020-09-19 16:14:43 -0500

    js sending info to html

--- chords.js
+++ chords.js
@@ -5,3 +5,4 @@ const majorChords = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 let randIndex = Math.floor(Math.random()*7);
 console.log(majorChords[randIndex]);
 
+document.getElementById("words").innerHTML = "5";
