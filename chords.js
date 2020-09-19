5f4eeb76f6a2590366331ec6b505115d6179b43d
Sofia-Ortega <sofia.j.ortega@tamu.edu>
2020-09-19 16:20:27 -0500

    outputting random chords

--- chords.js
+++ chords.js
@@ -1,8 +1,17 @@
 
+//.......................Arrays defining chords.....................
 const majorChords = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 
 
-let randIndex = Math.floor(Math.random()*7);
-console.log(majorChords[randIndex]);
+let chordCombo = "";
+let randIndex;
 
-document.getElementById("words").innerHTML = "5";
+
+for(let i = 0; i < majorChords.length; i++) {
+    randIndex = Math.floor(Math.random()*7);
+    chordCombo = chordCombo + majorChords[randIndex];
+}
+
+document.getElementById("words").innerHTML = chordCombo;
+
+
