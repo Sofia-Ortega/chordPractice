4fd3e60d4cc96ba43e5772ecd5a014333ffe8a35
Sofia-Ortega <sofia.j.ortega@tamu.edu>
2020-09-19 22:12:39 -0500

    got the button working

--- chords.js
+++ chords.js
@@ -5,27 +5,37 @@ const minorChords = ['Am', 'Bm', 'Cm', 'Dm', 'Em', 'Fm', 'Gm'];
 
 //.......................Defining variables..........................
 let chordCombo = "";
-let numOfChords = document.getElementById("chordNumber").value
-document.getElementById("testing").innerHTML = numOfChords
+var didUserClick = false;
+let numOfChords;
+
 
 //.......................Chord generator........................
-for(let i = 0; i < majorChords.length; i++) {
+function chordGen() {
+    let numOfChords = document.getElementById("chordNumber").value
+    document.getElementById("testing").innerHTML = numOfChords
 
-    let k = randNum(0,1);
-    if(k === 0) {
-        chordCombo = chordCombo + majorChords[randNum(0, 6)] + " ";
-    } else {
-        chordCombo = chordCombo + minorChords[randNum(0, 6)] + " ";
+    for (let i = 0; i < numOfChords; i++) {
+
+        let k = randNum(0, 1);
+        if (k === 0) {
+            chordCombo = chordCombo + majorChords[randNum(0, 6)] + " ";
+        } else {
+            chordCombo = chordCombo + minorChords[randNum(0, 6)] + " ";
+        }
+
     }
 
-}
-
 //.......................Output.......................................
-document.getElementById("chords").innerHTML = chordCombo;
+    document.getElementById("chords").innerHTML = chordCombo;
 
-
+}
 //........................Functions...............................
 //random number generator (start and end inclusive)
 function randNum(start, end) {
     return Math.floor((Math.random()*(end-start+1))+start)
+}
+
+//if user click, return userClick true
+function userClick() {
+    didUserClick = true;
 }
